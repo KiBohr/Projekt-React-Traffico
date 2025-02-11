9bbd2f7dfbcc89c899f9a5ff4103e7c5bc3e4b1d
David Schwarz <schwarz.david100@gmail.com>
2025-02-11 11:29:05 +0100

    Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,26 @@
+import Button from "./ui/button";
+
 export default function Hero() {
     return(
         <div>
-            <h1>
-                halllooooo
-            </h1>
+            
+            <section className="bg-contain bg-no-repeat  w-full h-[90vh]" style={{ backgroundImage: "url('/img/Scene.png')" }}>
+                <nav className="px-[16vw] py-[10vh] flex ">
+                    <img src="../../public/img/logo.png" alt="" />
+                    <article>
+                        <a href="">ABOUT</a>
+                        <a href="">HOW TO</a>
+                        <a href="">FAQS</a>
+                        <Button></Button>
+                    </article>
+                </nav>
+            </section>
+            
+            <section>
+                <img src="../../public/img/mobel.png" alt="" className="relative object-contain  h-[34vh]" />
+            </section>
         </div>
+
+
     )
 }
